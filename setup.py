9d3ba04aa8f30de9f50159e14e09449c65288ddf
ZJ Poh <jabryth@gmail.com>
2019-11-10 21:36:03 -0800

    Initial commit using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='my_word2vec',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My word2vec implementation in TensorFlow',
+    author='Zijie (ZJ) Poh',
+    license='MIT',
+)
